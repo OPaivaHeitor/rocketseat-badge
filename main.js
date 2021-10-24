e6c7072bc5a03c9cf41a81bfdbfde8111fae74c8
Heitor Sardinha <92725948+OPaivaHeitor@users.noreply.github.com>
2021-10-24 11:10:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const user = {
+  github: 'OPaivaHeitor',
+  youtube: '',
+  facebook: '',
+  instagram: 'meunome_heitor',
+  twitter: 'sardinhaheitor'
+}
+
+function changeSocialMediaLinks() {
+  userName.textContent = 'Heitor Paiva'
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://www.${social}.com/${user[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfo() {
+  const url = `https://api.github.com/users/${user.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userGithub.href = data.html_url
+      profilePic.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfo()
